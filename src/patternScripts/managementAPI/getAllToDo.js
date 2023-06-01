c4fc0cadd587a60112985e35725e18f92413a16a
Arthur Mendes <arthurmendesmartins0105@gmail.com>
2023-06-01 13:04:41 -0300

    trying fix status code 500 from vercel

--- src/patternScripts/managementAPI/getAllToDo.js
+++ src/patternScripts/managementAPI/getAllToDo.js
@@ -1,5 +1,5 @@
 async function getAllToDo() {
-    return await fetch('https://to-do-json-database.vercel.app/toDo').then(response => response.json())
+    return await fetch('https://to-do-json-database-rhl19x6mh-amendes.vercel.app/toDo').then(response => response.json())
 }
 
 export { getAllToDo }
